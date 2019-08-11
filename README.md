83ba3b1d3266bcabf5beb4a5f5746a1a81a5e45d
Fernando Cerveira <fernandocerveira@MacBook-Air-de-Fernando.local>
2019-08-11 02:00:20 -0300

    docs: adicionar documentação do código

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ArquiteturaDeSoftware
+# Atividade 15/8
+## Documentação da atividade
+
+A classe **Main** instancia duas classes: **LeitorDeTexto** e **AnalizadorDeTexto**.
+
+O construtor da classe **LeitorDeTexto** passa, por parâmetro, uma *string* com o caminho do arquivo no qual deseja ler. 
+> Um *arquivoDeExemplo.txt* é usado como padrão de exemplo do código.
+
+O construtor da classe **AnalizadorDeTexto** passa, por parâmetro, uma *string* para ser analizada e devolver as saídas:
+1. A string a ser analizada;
+2. O tamanho dela;
+3. Quantidade de vogais;
+4. Quantidade de espaços brancos;
+5. Quantidades de consoates.
+> Nesse exemplo a *string: String Exemplo* é passada como parâmetro.
--- src/main/java/com/fsma/arquitetura/main/Main.java
+++ src/main/java/com/fsma/arquitetura/main/Main.java
@@ -8,10 +8,10 @@ import com.fsma.arquitetura.arquivo.LeitorDeTexto;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-//		LeitorDeTexto leitorDeTexto = new LeitorDeTexto("/Users/fernandocerveira/Documents/AI/darknet/arq.txt");
-//		leitorDeTexto.lerArquivo();
+		LeitorDeTexto leitorDeTexto = new LeitorDeTexto("arquivoDeExemplo.txt");
+		leitorDeTexto.lerArquivo();
 		
-		AnalizadorDeTexto analizadorDeTexto = new AnalizadorDeTexto("Fernando Cerveira");
+		AnalizadorDeTexto analizadorDeTexto = new AnalizadorDeTexto("String Exemplo");
 		analizadorDeTexto.getTamanhoDaString();
 		analizadorDeTexto.getQuantidadeDeVogais();
 		analizadorDeTexto.getQuantidadeDeEspacosBrancos();
