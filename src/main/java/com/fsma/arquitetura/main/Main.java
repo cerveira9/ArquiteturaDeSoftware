bd7f3a94666c5cb0e539394ac7cfb0879c7e15ba
Fernando Cerveira <fernandocerveira@MacBook-Air-de-Fernando.local>
2019-08-11 00:16:11 -0300

    refactor: trocada lógica do leitor do arquivo

--- src/main/java/com/fsma/arquitetura/main/Main.java
+++ src/main/java/com/fsma/arquitetura/main/Main.java
@@ -1,10 +1,13 @@
 package com.fsma.arquitetura.main;
 
+import java.io.IOException;
+
+import com.fsma.arquitetura.leitor.LeitorDeTexto;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		LeitorDeTexto leitorDeTexto = new LeitorDeTexto("/Users/fernandocerveira/Documents/AI/darknet/arq.txt");
+		leitorDeTexto.lerArquivo();
 	}
-
 }
